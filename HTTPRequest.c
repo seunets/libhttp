b4d9063af35a487504fcff861a67587ce4ade372
Alexander Pereira Girald <alexander.girald@icloud.com>
2023-03-12 01:13:12 -0700

    Added URI decoding

--- HTTPRequest.c
+++ HTTPRequest.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "includes/HTTPRequest.h"
 
 
 static HTTPRequest_t * parse( HTTPRequest_t *request, char *message )
 {
-char *newLine, *strPtr;
+char *newLine, *strPtr, *p, *q;
 
    if( ( newLine = strstr( message, "\r\n" ) ) != NULL )
    {
@@ -23,6 +24,19 @@ char *newLine, *strPtr;
          if( ( request-> URI = strndup( message, ( size_t )( strPtr - message ) ) ) == NULL )
          {
             return NULL;
+         }
+
+         for( p = q = strchr( request-> URI, '%' ); p && *p; q = p )
+         {
+            if( isxdigit( p[ 1 ] ) && isxdigit( p[ 2 ] ) )
+            {
+               *q = ( char )( ( p[ 1 ] >= 'A' ? ( p[ 1 ] & 0xdf ) - 'A' + 10 : p[ 1 ] - '0' ) << 4 );
+               *q += p[ 2 ] >= 'A' ? ( p[ 2 ] & 0xdf ) - 'A' + 10 : p[ 2 ] - '0';
+            }
+            p = strchr( q, '%' );
+            p = p ? p : strchr( q, 0 );
+            strcpy( q + 1, q + 3 );
+            p -= 2;
          }
 
          if( strPtr == newLine && strcmp( request-> method, "GET" ) != 0 )
