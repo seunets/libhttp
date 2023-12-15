558a5dd3ef3b091c4d1851729652e8f5fb4636ae
Alexander Pereira Girald <alexander.girald@icloud.com>
2023-12-15 12:58:46 -0700

    Create Message.c

---
+++ Message.c
@@ -0,0 +1,91 @@
+#include "includes/Message.h"
+#include <stdio.h>
+
+
+static void delete( Message_t *this )
+{
+   free( this-> pdu );
+   free( this );
+}
+
+
+static int send_( const Message_t *this, int clientSocket )
+{
+   send( clientSocket, this-> pdu, this-> size, 0 );
+   return errno;
+}
+
+
+static void *reassemble( char *message, char *fragment, size_t * messageLength, ssize_t fragmentSize )
+{
+void *tmp;
+
+   if( ( tmp = realloc( message, ( size_t ) fragmentSize + *messageLength + 1 ) ) == NULL )
+   {
+      return NULL;
+   }
+   else
+   {
+      message = tmp;
+   }
+
+   memmove( message + *messageLength, fragment, ( size_t ) fragmentSize );
+   *messageLength += ( size_t ) fragmentSize;
+   message[ *messageLength ] = '\0';
+   return message;
+}
+
+
+static int receive( Message_t *this, int clientSocket )
+{
+char buffer[ BUFFERSIZE ];
+ssize_t bytesRead;
+
+   this-> size = 0;
+   if( ( bytesRead = recv( clientSocket, buffer, BUFFERSIZE, 0 ) ) <= 0 )
+   {
+      free( this );
+   }
+   else
+   {
+      if( ( this-> pdu = calloc( 1, this-> size ) ) == NULL )
+      {
+         free( this );
+      }
+      else
+      {
+         if( ( this-> pdu = reassemble( this-> pdu, buffer, &this-> size, bytesRead ) ) == NULL )
+         {
+            free( this );
+            this = NULL;
+         }
+         else
+         {
+            while( ( bytesRead = recv( clientSocket, buffer, BUFFERSIZE, MSG_DONTWAIT ) ) > 0 )
+            {
+               if( ( this-> pdu = reassemble( this-> pdu, buffer, &this-> size, bytesRead ) ) == NULL )
+               {
+                  free( this );
+                  this = NULL;
+               }
+            }
+         }
+      }
+   }
+   return errno;
+}
+
+
+Message_t *Message_new( void )
+{
+Message_t *this;
+
+   if( ( this = calloc( 1, sizeof( Message_t ) ) ) != NULL )
+   {
+      this-> send = send_;
+      this-> receive = receive;
+      this-> delete = delete;
+   }
+
+   return this;
+}
