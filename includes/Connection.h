9ee6ab49111dd56edaa7f3b66acc49ddf402fa86
Alexander Pereira Girald <alexander.girald@icloud.com>
2023-12-15 12:56:21 -0700

    Create Connection.h

---
+++ includes/Connection.h
@@ -0,0 +1,27 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#include <sys/event.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <signal.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include "Message.h"
+#include <unistd.h>
+
+typedef struct connection
+{
+   char *peer;
+   Message_t *message;
+   int socket;
+   int kq;
+   struct kevent *event;
+   int ( *accept )( struct connection * );
+   int ( *establish )( struct connection * );
+   void ( *delete )( struct connection * );
+} Connection_t;
+
+Connection_t *Connection_new( const char *, const int );
+
+#endif
