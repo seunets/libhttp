d74ba2169bef6f3e47631d0d6359ff1a6991718c
Alexander Pereira Girald <alexander.girald@icloud.com>
2023-12-15 12:52:54 -0700

    Refactor HTTPResponse.h

--- includes/HTTPResponse.h
+++ includes/HTTPResponse.h
@@ -1,17 +1,20 @@
 #ifndef HTTP_RESPONSE_H
 #define HTTP_RESPONSE_H
 
+#include "Message.h"
 #include "HTTPHeader.h"
 
 typedef struct http_response
 {
    char *version;
-   char *code;
+   unsigned int code;
+   char pad[ 4 ];
    char *reason;
    HTTPHeader_t *headers;
    char *body;
-   struct http_response * ( *parse )( struct http_response *, char * );
-   void ( *delete )( const struct http_response * );
+   struct http_response * ( *parse )( struct http_response *, Message_t * );
+   Message_t * ( *serialize )( const struct http_response * );
+   void ( *delete )( struct http_response * );
 } HTTPResponse_t;
 
 HTTPResponse_t *HTTPResponse_new( void );
