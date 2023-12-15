2931c3c6c60a91cff0cb6083f166963fc9e71247
Alexander Pereira Girald <alexander.girald@icloud.com>
2023-12-15 12:51:59 -0700

    Refactor HTTPRequest.h

--- includes/HTTPRequest.h
+++ includes/HTTPRequest.h
@@ -2,8 +2,9 @@
 #define HTTP_REQUEST_H
 
 #include <arpa/inet.h>
+#include <ctype.h>
 #include "HTTPHeader.h"
-
+#include "Message.h"
 
 typedef struct http_request
 {
@@ -13,8 +14,10 @@ typedef struct http_request
    HTTPHeader_t *headers;
    char *body;
    char remoteAddr[ INET6_ADDRSTRLEN ];
-   struct http_request * ( *parse )( struct http_request *, char * );
-   void ( *delete )( const struct http_request * );
+   char pad[ 2 ];
+   struct http_request * ( *parse )( struct http_request *, Message_t * );
+   const Message_t * ( *serialize )( const struct http_request * );
+   void ( *delete )( struct http_request * );
 } HTTPRequest_t;
 
 HTTPRequest_t *HTTPRequest_new( void );
