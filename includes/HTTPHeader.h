d7494414cfbb8259b74ae1f77ba95f65e37bcde5
Alexander Pereira Girald <alexander.girald@icloud.com>
2023-12-15 12:51:19 -0700

    Refactor HTTPHeader.h

--- includes/HTTPHeader.h
+++ includes/HTTPHeader.h
@@ -1,13 +1,19 @@
 #ifndef HTTP_HEADER_H
 #define HTTP_HEADER_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 typedef struct http_header
 {
    char **keys;
    char **values;
    int ( *set )( struct http_header *, const char *, const char * );
    const char * ( *get )( const struct http_header *, const char * );
-   void ( *delete )( const struct http_header * );
+   void ( *parse )( struct http_header *, const char *, const char * );
+   const char * ( *serialize )( const struct http_header * );
+   void ( *delete )( struct http_header * );
 } HTTPHeader_t;
 
 HTTPHeader_t *HTTPHeader_new( void );
